69cd3baf5285ff09c8479b4434c5c4118140f6c3
unknown <houj@oxy.edu>
2020-04-13 13:05:46 -0700

    Added functionality for filtering by Completed and Uncompleted

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo)
 
 //Functions
 function addTodo(event){
@@ -52,3 +54,35 @@ function deleteCheck(e){
     }
 }
 
+function filterTodo(e){
+    console.log(todoList.childNodes);
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        
+        switch(e.target.value){
+            case "all":
+                if(todo.style!=null){
+                    todo.style.display="flex";
+                }
+                break;
+            case "completed":
+                if(todo.style!=null){
+                    if(todo.classList.contains("completed")){
+                        todo.style.display="flex";
+                    }else{
+                        todo.style.display= "none";
+                    }
+                }
+                break;
+            case "uncompleted":
+                if(todo.style!=null){
+                    if(!todo.classList.contains("completed")){
+                        todo.style.display="flex";
+                    }else{
+                        todo.style.display= "none";
+                    }
+                }
+                break;
+        }
+    });
+}
